959abac2b9c3d53e85e1d969e62b8bd8925901c6
nkgevorgyan <nkgevorgyan@gmail.com>
2023-04-30 16:23:30 +0200

    add some unimportant lines

--- lua/narek/core/options.lua
+++ lua/narek/core/options.lua
@@ -10,9 +10,16 @@ opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
+
+-- theprimeeagen
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
